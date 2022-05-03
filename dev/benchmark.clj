f2cd5d29ca3f78b96d7114378338824389279327
Luc Engelen <16940687+ljpengelen@users.noreply.github.com>
2022-05-03 09:20:27 +0200

    Leave JSON parsing out of benchmark

--- dev/benchmark.clj
+++ dev/benchmark.clj
@@ -1,28 +1,27 @@
 (ns benchmark
-  (:require [clojure.data.json :as json]
-            [criterium.core :refer [quick-bench]]
+  (:require [criterium.core :refer [quick-bench]]
             [org.httpkit.client :as client]))
 
 (defn http-request
   ([method endpoint] (http-request method endpoint nil))
   ([method endpoint body]
-   (-> @(client/request {:url (str "http://localhost:3000/" endpoint)
-                         :method method
-                         :headers {"Content-type" "application/json"}
-                         :body (when body (json/write-str body))})
-       :body
-       (json/read-str :key-fn keyword))))
+   @(client/request {:url (str "http://localhost:3000/" endpoint)
+                     :method method
+                     :headers {"Content-type" "application/json"}
+                     :body body})))
 
 (comment
-  ;; Execution time mean : 1,084466 ms
-  (quick-bench (http-request :post "account" {:name "Benchmark"}))
-  ;; Execution time mean : 275,773633 µs
+  ;; Execution time mean : 87,820996 µs
+  (quick-bench (http-request :post "account" "{\"name\": \"Benchmark\"}"))
+  ;; Execution time mean : 88,185795 µs
   (quick-bench (http-request :get "account/1"))
-  ;; Execution time mean : 1,359019 ms
-  (quick-bench (http-request :post "account/1/deposit" {:amount 10}))
-  ;; Execution time mean : 1,584553 ms
-  (quick-bench (http-request :post "account/1/withdraw" {:amount 1}))
-  ;; Execution time mean : 1,383149 ms
-  (quick-bench (http-request :post "account/1/send" {:account-number 2 :amount 1}))
-  ;; Execution time mean : 120,300763 ms
-  (quick-bench (http-request :get "account/1/audit")))
+  ;; Execution time mean : 90,140511 µs
+  (quick-bench (http-request :post "account/1/deposit" "{\"amount\": 10}"))
+  ;; Execution time mean : 90,270936 µs
+  (quick-bench (http-request :post "account/1/withdraw" "{\"amount\": 1}"))
+  ;; Execution time mean : 89,329678 µs
+  (quick-bench (http-request :post "account/1/send" "{\"account-number\": 2, \"amount\": 1}"))
+  ;; Execution time mean : 88,261390 µs
+  (quick-bench (http-request :get "account/1/audit"))
+  ;; Execution time mean : 90,320477 µs
+  (quick-bench (http-request :get "account/3/audit")))
